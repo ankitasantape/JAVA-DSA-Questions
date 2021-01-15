4629373e4cfe7ce2366b4f6c7dfa5201122d5573
ankita S <54230924+ankitasantape@users.noreply.github.com>
2021-01-15 18:29:13 +0530

    Create StudentUse.java

---
+++ OOPS 1/Introduction to OOPS/StudentUse.java
@@ -0,0 +1,22 @@
+package classes_and_objects;
+
+import java.util.Scanner;
+
+public class StudentUse {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		Student s1 = new Student();
+		s1.name = "Ankush";
+		s1.rollNumber = 123;
+		
+		Student s2 = new Student();
+		s2.name = "Manisha";
+		s2.rollNumber = 121;
+		
+		System.out.println(s1.name);
+		System.out.println(s2.name);
+		//System.out.println(s1);
+	}
+
+}
