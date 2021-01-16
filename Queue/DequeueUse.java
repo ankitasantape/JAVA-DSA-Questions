c7ce0ac37b6f5a26f29899d7753c9dcbc444eb73
ankita S <54230924+ankitasantape@users.noreply.github.com>
2021-01-16 20:43:38 +0530

    Create  DequeueUse.java

---
+++ Queue/DequeueUse.java
@@ -0,0 +1,58 @@
+package Queue;
+
+import java.util.Scanner;
+
+public class DequeueUse {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);	
+		 
+		Dequeue queue = new Dequeue(10);
+
+		while ( true ) {
+			
+			int choice = s.nextInt(), input;
+			
+			switch(choice) {
+				case 1:
+					    input = s.nextInt();
+					    queue.insertFront(input);
+					    break;
+
+				case 2:
+					    input = s.nextInt();
+					    queue.insertRear(input);
+					    break;
+
+				case 3:
+					    queue.deleteFront();
+					    break;
+
+				case 4:
+					    queue.deleteRear();
+					    break;
+				
+				case 5:
+				        System.out.println(queue.getFront());
+				        break;
+				
+				case 6:
+				     
+					    System.out.println(queue.getRear());
+			            break;
+
+				default: 
+					   
+					    return;
+			}
+
+			s.close();
+		}
+		
+		
+	}
+	
+	
+
+}
