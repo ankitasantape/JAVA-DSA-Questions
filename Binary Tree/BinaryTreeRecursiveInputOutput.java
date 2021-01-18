a2e0df0fb7b28404987866a4b0165b039bb49134
ankita S <54230924+ankitasantape@users.noreply.github.com>
2021-01-18 16:22:56 +0530

    Update BinaryTreeRecursiveInputOutput.java

--- Binary Tree/BinaryTreeRecursiveInputOutput.java
+++ Binary Tree/BinaryTreeRecursiveInputOutput.java
@@ -1,32 +1,74 @@
 package BinaryTree;
 
 import java.util.Scanner;
-
+/*
+Input:
+Enter root data: 
+1
+Enter root data: 
+2
+Enter root data: 
+4
+Enter root data: 
+-1
+Enter root data: 
+-1
+Enter root data: 
+5
+Enter root data: 
+-1
+Enter root data: 
+-1
+Enter root data: 
+3
+Enter root data: 
+6
+Enter root data: 
+-1
+Enter root data: 
+-1
+Enter root data: 
+7
+Enter root data: 
+-1
+Enter root data: 
+-1
+Output:
+1: L: 2,R: 3,
+2: L: 4,R: 5,
+4: 
+5: 
+3: L: 6,R: 7,
+6: 
+7: 
+*/
 public class BinaryTreeRecursiveInputOutput
 {
     
 	public static void print( BinaryTreeNode<Integer> root ) {
-		
+		//if root ka data null hai to directly return kro
 		if ( root == null ) {
 			
 			return;
 		}
-		
+		//if root ka data null nhi hai toh string me append krke uske left and right part ko explore kro
 		String toBePrinted = root.data +  ": ";
+		
 		
 		if ( root.left != null ) {
 			
 			toBePrinted += "L: " + root.left.data + ",";
 		}
 		
-		if (root.right != null) {
+		if ( root.right != null ) {
 
 			toBePrinted += "R: " + root.right.data + ",";
 		}
 		
 		System.out.println(toBePrinted);
-		
+		//Phle left part complete krenge, root ka left input lenge then root ke left ke left ko and right ko input krenge then left ke left ke left and right ko input krenge
 		print( root.left );
+		//after completing whole left part then right part start krenge, first root ka right input krenge then right ke right ko input krenge then left ko input krenge  
 		print( root.right );
 	}
 	public static BinaryTreeNode<Integer> takeInput( Scanner s ) {
