9962ab1a44e48938946d62899ce0912c733cc399
ankita S <54230924+ankitasantape@users.noreply.github.com>
2021-01-19 13:29:07 +0530

    Update HeightOfBinaryTree.java

--- Binary Tree/HeightOfBinaryTree.java
+++ Binary Tree/HeightOfBinaryTree.java
@@ -64,45 +64,6 @@ public class HeightOfBinaryTree {
 		
 		System.out.println(height);
 	}
-    
-	public static void printLevelWise(BinaryTreeNode<Integer> root) {
-		
-		   Queue<BinaryTreeNode<Integer>> queue = new LinkedList<>();
-			
-		   queue.add(root);
-		  // queue.add(null);
-			
-		   while ( !queue.isEmpty() ) {
-			    
-			   BinaryTreeNode<Integer> frontNode = queue.poll();
-			   
-			   System.out.print(frontNode.data +":");
-			  
-			   if ( frontNode.left != null) {
-
-					System.out.print("L:" + frontNode.left.data + ",");
-					queue.add(frontNode.left);
-			   } 
-			   else
-			   {
-				   System.out.print("L:"+ -1 + ",");
-			   }
-
-				if ( frontNode.right != null) {
-
-					System.out.println("R:" + frontNode.right.data );
-					queue.add(frontNode.right);
-				}
-			    else 
-			    {
-				    System.out.print("R:" + -1 );
-			    }
-			    
-				System.out.println();
-		   }
-			
-	}
-
 	public static BinaryTreeNode<Integer> takeInput( ) {
 		
 		Scanner s = new Scanner(System.in);
